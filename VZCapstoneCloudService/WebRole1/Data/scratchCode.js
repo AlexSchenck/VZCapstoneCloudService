50aed819fbe19f58bf63653c383d4684c60c6eea
Alex Schenck <AlexNSchenck@gmail.com>
2016-04-30 14:27:37 -0700

    readded current website

---
+++ VZCapstoneCloudService/WebRole1/Data/scratchCode.js
@@ -0,0 +1,28 @@
+
+var margin = {top: 250, right: 40, bottom: 250, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.time.scale()
+    .domain([new Date(2012, 0, 1), new Date(2020, 0, 1)])
+    .range([0, width]);
+
+var xAxis = d3.svg.axis()
+    .scale(x);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+  .selectAll("text")
+    .attr("y", 0)
+    .attr("x", 9)
+    .attr("dy", ".35em")
+    .attr("transform", "rotate(90)")
+    .style("text-anchor", "start");
