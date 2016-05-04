535803074334cd127c50a75ddab06505b7041abd
MadisonIkeda <maikeda@uw.edu>
2016-05-03 23:18:14 -0700

    first full draft

--- VZCapstoneCloudService/WebRole1/Scripts/stats.js
+++ VZCapstoneCloudService/WebRole1/Scripts/stats.js
@@ -10,7 +10,7 @@ var svg = d3.select("#currentFatal")
     		.attr("width", width + padding.left + padding.right)
     		.attr("height", height + padding.top + padding.bottom);
 svg.append("text")
-    .attr("x", 10)
+    .attr("x", 95)
     .attr("y", 20)
     .attr("dy", ".95em")
     .text("17");
@@ -21,7 +21,27 @@ var goal = d3.select("#goal")
     		.attr("width", width + padding.left + padding.right)
     		.attr("height", height + padding.top + padding.bottom);
 goal.append("text")
-    .attr("x", 10)
+    .attr("x", 105)
     .attr("y", 20)
     .attr("dy", ".95em")
     .text("0");
+
+d3.select("#ped")
+            .append("svg")
+            .attr("width", width + padding.left + padding.right)
+            .attr("height", height + padding.top + padding.bottom)
+            .append("text")
+    .attr("x", 60)
+    .attr("y", 20)
+    .attr("dy", ".95em")
+    .text("9 / 10");
+
+d3.select("#cycle")
+            .append("svg")
+            .attr("width", width + padding.left + padding.right)
+            .attr("height", height + padding.top + padding.bottom)
+            .append("text")
+    .attr("x", 60)
+    .attr("y", 20)
+    .attr("dy", ".95em")
+    .text("7 / 10");
