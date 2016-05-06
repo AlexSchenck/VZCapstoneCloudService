43c058f06291f4fce12c334d273278e24d4354b6
hmcdonough <hmcd311@gmail.com>
2016-05-06 14:04:10 -0700

    added popup

--- VZCapstoneCloudService/WebRole1/Scripts/map.js
+++ VZCapstoneCloudService/WebRole1/Scripts/map.js
@@ -1,15 +1,18 @@
 require([
     "esri/map",
-    "esri/layers/FeatureLayer"
-], function (Map, FeatureLayer) {
+    "esri/layers/FeatureLayer",
+    "esri/InfoTemplate"
+], function (Map, FeatureLayer, InfoTemplate) {
     var map = new Map("mapid", {
-        basemap: 'topo',
+        basemap: 'streets-navigation-vector',
         sliderOrientation : "horizontal",
         center: [-122.3321, 47.6062],
-        zoom: 13
+        zoom: 15
     });
 
-    var featureLayer = new FeatureLayer("http://gisrevprxy.seattle.gov/arcgis/rest/services/SDOT_EXT/DSG_datasharing/MapServer/51");
+    var featureLayer = new FeatureLayer("http://gisrevprxy.seattle.gov/arcgis/rest/services/SDOT_EXT/DSG_datasharing/MapServer/51", {
+        infoTemplate: new InfoTemplate("Collision:", "${*}") // "${*}")
+    });
 
     map.addLayer(featureLayer);
 });
